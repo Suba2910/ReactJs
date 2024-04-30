098ed1e1cacd09d1bd25c43ac890336fb9fb0455
Suba2910 <subalakshmishan@gmail.com>
2024-04-30 14:07:51 +1000

    Initial commit

---
+++ episode11/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Forms } from './components/Forms';
+
+
+
+// code to demonstrate react hook forms
+function App() {
+  return (<div className = 'App'>
+<Forms/>
+  </div>
+  );
+
+}
+
+
+
+
+
+export default App
+
+
